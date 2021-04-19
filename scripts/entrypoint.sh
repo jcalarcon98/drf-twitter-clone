08376bde1cb1ba0973ee9ce05fc0c212d9362635
Jean Carlos Alarcón <jeancalarcon98@gmail.com>
2021-04-19 01:05:34 -0500

    Fix Apply migrations command on entrypoint.sh script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 
 python manage.py collectstatic --noinput
 
-python manage makemigrations
+python manage.py makemigrations
 
-python manage migrate
+python manage.py migrate
 
 uwsgi --socket :8000 --master --enable-threads --module config.wsgi
